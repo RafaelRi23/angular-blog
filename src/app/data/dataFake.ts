687ae3a908edca87122bcdb9e6dfe371cca63833
Rafael Ribeiro <ribeirorafael8814@gmail.com>
2023-10-15 15:13:56 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "Captain Tsubasa",
+    "description": "Vamos",
+    "photoCover": "https://www.tsubasa-dreamteam.com/images/system/pc/banner_220325_gl.jpg"
+  }, 
+  {
+    "id":"2",
+    "title": "Inazuma Eleven",
+    "description": "ola",
+    "photoCover": "https://universonintendo.com/wp-content/uploads/2022/12/Inazuma-Eleven-Victory-Road-of-Heroes-Scrn28122022-.jpg"
+  }
+]
